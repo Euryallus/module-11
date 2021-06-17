0d2741981510dcf3f3ce70e830c6afff5c588330
JoeAllen7 <Joseph.Allen@mail.bcu.ac.uk>
2021-06-17 16:54:38 +0100

    freeze/slam interaction fixes
    - Frozen enemies are now unaffected by the slam ability
    - Using the freeze ability on enemies in mid-air will drop and kill them

--- Module10/Assets/Scripts/Joe Scripts/Abilities/SlamAbility.cs
+++ Module10/Assets/Scripts/Joe Scripts/Abilities/SlamAbility.cs
@@ -19,15 +19,17 @@ public class SlamAbility : PlayerAbility
     [SerializeField] private CameraShake    playerCameraShake;      // Reference to the script on the player that handles camera shake
     [SerializeField] private float          effectRadius = 15.0f;
     [SerializeField] private float          enemyLiftHeight = 6.0f;
+    [SerializeField] private float          enemyDropDamage = 0.5f;
 
     private SlamAbilityState    abilityState        = SlamAbilityState.None;
     private List<EnemyBase>     targetedEnemies     = new List<EnemyBase>();
     private List<Vector3>       enemyStartPositions = new List<Vector3>();
     private List<Vector3>       enemyLiftPositions  = new List<Vector3>();
-    private float               lerpProgress;
+    //private float               lerpProgress;
 
     private const float LiftSpeed = 1.5f;
     private const float DropSpeed = 1.5f;
+    private const float MaxRandomDropDelay = 0.2f;
 
     protected override void AbilityStart()
     {
@@ -37,70 +39,98 @@ public class SlamAbility : PlayerAbility
         base.AbilityStart();
 
         StartCoroutine(AbilityEffectCoroutine());
-
-        LayerMask layerMask = LayerMask.GetMask("Enemy");
-
-        Collider[] collidersInRadius = Physics.OverlapSphere(transform.position, effectRadius, layerMask);
-
-        targetedEnemies     .Clear();
-        enemyStartPositions .Clear();
-        enemyLiftPositions  .Clear();
-
-        abilityState = SlamAbilityState.Starting;
-
-        foreach (Collider collider in collidersInRadius)
-        {
-            EnemyBase enemyScript = collider.GetComponent<EnemyBase>();
-
-            if(enemyScript != null)
-            {
-                Debug.Log("Slam ability targeted enemy: " + collider.gameObject.name, collider.gameObject);
-
-                enemyScript.StopAgentMovement();
-
-                targetedEnemies.Add(enemyScript);
-
-                Vector3 enemyPos = collider.gameObject.transform.position;
-
-                enemyStartPositions.Add(enemyPos);
-                enemyLiftPositions.Add(new Vector3(enemyPos.x, enemyPos.y + enemyLiftHeight, enemyPos.z));
-            }
-        }
     }
 
     private IEnumerator AbilityEffectCoroutine()
     {
+        // Starting (Find enemies + initial VFX)
+        //=======================================
+
+        abilityState = SlamAbilityState.Starting;
+
+        ApplyEffectToEnemiesInRadius();
+
         Instantiate(shockwaveEffectPrefab, transform);
 
         playerCameraShake.ShakeCameraForTime(1.2f, CameraShakeType.ReduceOverTime, 0.07f);
 
         yield return new WaitForSeconds(0.3f);
 
-        Instantiate(impactEffectPrefab, transform);
+        // Lifting + secondary VFX
+        //=========================
 
         abilityState = SlamAbilityState.LiftingEnemies;
 
+        Instantiate(impactEffectPrefab, transform);
+
         yield return new WaitForSeconds(6.0f);
 
-        lerpProgress = 0.0f;
+        // Dropping
+        //==========
+
+        //lerpProgress = 0.0f;
 
         abilityState = SlamAbilityState.DroppingEnemies;
+
+        // Drop any enemies that were not already dropped due to external factors (e.g. freezing a suspended enemy)
+        DropRemainingEnemies();
+
+        yield return new WaitForSeconds(DropSpeed + MaxRandomDropDelay);
+
+        abilityState = SlamAbilityState.None;
     }
 
     protected override void Update()
     {
         base.Update();
 
-        switch (abilityState)
+        if(abilityState == SlamAbilityState.LiftingEnemies)
         {
-            case SlamAbilityState.LiftingEnemies:
-                LiftEnemiesUpdate();
-                break;
-
-            case SlamAbilityState.DroppingEnemies:
-                DropEnemiesUpdate();
-                break;
+            LiftEnemiesUpdate();
         }
+    }
+
+    private void ApplyEffectToEnemiesInRadius()
+    {
+        LayerMask layerMask = LayerMask.GetMask("Enemy");
+
+        Collider[] collidersInRadius = Physics.OverlapSphere(transform.position, effectRadius, layerMask);
+
+        targetedEnemies.Clear();
+        enemyStartPositions.Clear();
+        enemyLiftPositions.Clear();
+
+        foreach (Collider collider in collidersInRadius)
+        {
+            EnemyBase enemyScript = collider.GetComponent<EnemyBase>();
+
+            if (enemyScript != null)
+            {
+                Debug.Log("Slam ability targeted enemy: " + collider.gameObject.name, collider.gameObject);
+
+                if (enemyScript.AgentEnabled)
+                {
+                    // Only apply effect to enemies that don't already have their agents disabled
+                    //   (e.g. enemies that are frozen will not be affected)
+
+                    ApplyEffectToEnemy(enemyScript, collider);
+                }
+            }
+        }
+    }
+
+    private void ApplyEffectToEnemy(EnemyBase enemy, Collider enemyCollider)
+    {
+        enemy.StopAgentMovement();
+
+        enemy.Suspended = true;
+
+        targetedEnemies.Add(enemy);
+
+        Vector3 enemyPos = enemyCollider.gameObject.transform.position;
+
+        enemyStartPositions.Add(enemyPos);
+        enemyLiftPositions.Add(new Vector3(enemyPos.x, enemyPos.y + enemyLiftHeight, enemyPos.z));
     }
 
     private void LiftEnemiesUpdate()
@@ -116,41 +146,75 @@ public class SlamAbility : PlayerAbility
         }
     }
 
-    private void DropEnemiesUpdate()
-    {
-        if(lerpProgress < 1.0f)
-        {
-            for (int i = 0; i < targetedEnemies.Count; i++)
-            {
-                if (targetedEnemies[i] != null)
-                {
-                    GameObject enemyGameObj = targetedEnemies[i].gameObject;
+    //private void DropEnemiesUpdate()
+    //{
+    //    if(lerpProgress < 1.0f)
+    //    {
+    //        for (int i = 0; i < targetedEnemies.Count; i++)
+    //        {
+    //            if (targetedEnemies[i] != null)
+    //            {
+    //                GameObject enemyGameObj = targetedEnemies[i].gameObject;
 
-                    // Cubing lerpProgress to give ease-in effect for drop, mimicking gravity
-                    enemyGameObj.transform.position = Vector3.Lerp(enemyLiftPositions[i], enemyStartPositions[i], lerpProgress * lerpProgress * lerpProgress);
-                }
-            }
-        }
-        else
-        {
-            EnemyEffectsDone();
-        }
+    //                // Cubing lerpProgress to give ease-in effect for drop, mimicking gravity
+    //                enemyGameObj.transform.position = Vector3.Lerp(enemyLiftPositions[i], enemyStartPositions[i], lerpProgress * lerpProgress * lerpProgress);
+    //            }
+    //        }
+    //    }
+    //    else
+    //    {
+    //        EnemyEffectsDone();
+    //    }
 
-        lerpProgress += Time.deltaTime * DropSpeed;
-    }
+    //    lerpProgress += Time.deltaTime * DropSpeed;
+    //}
 
-    private void EnemyEffectsDone()
+    private void DropRemainingEnemies()
     {
         for (int i = 0; i < targetedEnemies.Count; i++)
         {
-            if (targetedEnemies[i] != null)
-            {
-                targetedEnemies[i].gameObject.transform.position = enemyStartPositions[i];
-                targetedEnemies[i].StartAgentMovement();
-            }
+            StartCoroutine(DropEnemyCoroutine(i, Random.Range(0.0f, MaxRandomDropDelay)));
         }
+    }
 
-        abilityState = SlamAbilityState.None;
+    public void DropAndKillSuspendedEnemy(EnemyBase enemy)
+    {
+        if(enemy.Suspended && targetedEnemies.Contains(enemy))
+        {
+            // Drop the enemy and apply a huge amount of damage to kill it once it lands
+            StartCoroutine(DropEnemyCoroutine(targetedEnemies.IndexOf(enemy), 0.0f, 100.0f));
+        }
+    }
+
+    private IEnumerator DropEnemyCoroutine(int enemyIndex, float startDelay, float damageOverride = -1.0f)
+    {
+        if(targetedEnemies[enemyIndex] != null)
+        {
+            EnemyBase enemyToDrop = targetedEnemies[enemyIndex];
+            
+            enemyToDrop.Suspended = false;
+
+            yield return new WaitForSeconds(startDelay);
+
+            float dropProgress = 0.0f;
+
+            while(dropProgress < 1.0f)
+            {
+                enemyToDrop.gameObject.transform.position = Vector3.Lerp(enemyLiftPositions[enemyIndex], enemyStartPositions[enemyIndex], dropProgress * dropProgress * dropProgress);
+
+                dropProgress += Time.deltaTime * DropSpeed;
+
+                yield return null;
+            }
+
+            float enemyDamage = (damageOverride == -1.0f ? enemyDropDamage : damageOverride);
+
+            enemyToDrop.gameObject.GetComponent<EnemyHealth>().DoDamage(enemyDamage);
+
+            enemyToDrop.StartAgentMovement();
+
+            targetedEnemies[enemyIndex] = null;
+        }
     }
 
     protected override void FindUIIndicator()
